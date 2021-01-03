c7f7e5c08dfb85bbaf03199780bdd776b86d716b
alex kozlov <ykozlov@gmail.com>
2021-01-02 20:04:15 -0500

    Add delete k8s script

---
+++ scripts/deleteToKubernetes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "${REPOSITORY_PREFIX}" ]
+then 
+    echo "Please set the REPOSITORY_PREFIX"
+else 
+    if [ -z "${IMAGE_SUFFIX}" ]
+    then 
+        echo "Please set the IMAGE_SUFFIX"
+    else 
+        cat ./k8s/*.yaml | \
+        sed 's#\${REPOSITORY_PREFIX}'"#${REPOSITORY_PREFIX}#g" | \
+        sed 's#\${IMAGE_SUFFIX}'"#${IMAGE_SUFFIX}#g" | \
+        kubectl delete -f -
+    fi
+fi

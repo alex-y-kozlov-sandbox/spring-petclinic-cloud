963f1e08d355cfe9ee35f595caa6b3b8ed99a21f
alex kozlov <ykozlov@gmail.com>
2021-01-02 19:37:41 -0500

    Add dd agent

--- scripts/deployToKubernetes.sh
+++ scripts/deployToKubernetes.sh
@@ -4,7 +4,13 @@ if [ -z "${REPOSITORY_PREFIX}" ]
 then 
     echo "Please set the REPOSITORY_PREFIX"
 else 
-    cat ./k8s/*.yaml | \
-    sed 's#\${REPOSITORY_PREFIX}'"#${REPOSITORY_PREFIX}#g" | \
-    kubectl apply -f -
+    if [ -z "${IMAGE_SUFFIX}" ]
+    then 
+        echo "Please set the IMAGE_SUFFIX"
+    else 
+        cat ./k8s/*.yaml | \
+        sed 's#\${REPOSITORY_PREFIX}'"#${REPOSITORY_PREFIX}#g" | \
+        sed 's#\${IMAGE_SUFFIX}'"#${IMAGE_SUFFIX}#g" | \
+        kubectl apply -f -
+    fi
 fi
--- scripts/pushImages.sh
+++ scripts/pushImages.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-docker push ${REPOSITORY_PREFIX}/api-gateway:latest
-docker push ${REPOSITORY_PREFIX}/visits-service:latest
-docker push ${REPOSITORY_PREFIX}/vets-service:latest
-docker push ${REPOSITORY_PREFIX}/customers-service:latest
-docker push ${REPOSITORY_PREFIX}/admin-server:latest
-docker push ${REPOSITORY_PREFIX}/discovery-service:latest
-docker push ${REPOSITORY_PREFIX}/config-server:latest
+docker push ${REPOSITORY_PREFIX}/api-gateway${IMAGE_SUFFIX}:latest
+docker push ${REPOSITORY_PREFIX}/visits-service${IMAGE_SUFFIX}:latest
+docker push ${REPOSITORY_PREFIX}/vets-service${IMAGE_SUFFIX}:latest
+docker push ${REPOSITORY_PREFIX}/customers-service${IMAGE_SUFFIX}:latest
+docker push ${REPOSITORY_PREFIX}/admin-server${IMAGE_SUFFIX}:latest
+docker push ${REPOSITORY_PREFIX}/discovery-service${IMAGE_SUFFIX}:latest
+docker push ${REPOSITORY_PREFIX}/config-server${IMAGE_SUFFIX}:latest
+
+
 
